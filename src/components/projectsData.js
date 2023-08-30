4348fb07c3e692f3f5faeed50af9ce2800b14344
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-30 13:24:53 +0300

    media query navbar links routers style

---
+++ src/components/projectsData.js
@@ -0,0 +1,52 @@
+const projectsData = [
+  {
+    title: "Humming Bird",
+    description: "A stylish and interactive web app showcasing captivating visuals and seamless user experiences.",
+    image: "https://i.pinimg.com/564x/bc/9a/3e/bc9a3effc2bc60ecab3797c7f240a6c1.jpg",
+    technologies: ["React", "Node.js"],
+  },
+  {
+    title: "E-Commerce Website",
+    description: "An engaging e-commerce platform featuring product listings, carts, and secure payment processing.",
+    image: "https://i.pinimg.com/564x/fc/35/f3/fc35f34f1bce69afab64c82f18c14298.jpg",
+    technologies: ["React", "Node.js"],
+  },
+  {
+    title: "Travel Blog App",
+    description: "A dynamic travel blog application allowing users to share their journeys through multimedia-rich posts.",
+    image: "https://i.pinimg.com/originals/43/69/c8/4369c895e43b83bf3ff495f7c34678a6.gif",
+    technologies: ["React", "Express"],
+  },
+  {
+    title: "SUMMER CAMP'21",
+    description: "A captivating web platform for an event, offering engaging activities and informative content.",
+    image: "https://i.pinimg.com/564x/2c/e2/a7/2ce2a7edea0aabbf0569b26ca2aa96ba.jpg",
+    technologies: ["React", "Flask"],
+  },
+  {
+    title: "Nature Protection",
+    description: "A web application dedicated to raising awareness about nature conservation and eco-friendly practices.",
+    image: "https://i.pinimg.com/564x/4f/23/b9/4f23b935c975ca37fd4e159ea5d74740.jpg",
+    technologies: ["React", "Rails"],
+  },
+  {
+    title: "SUMMER CAMP'21",
+    description: "An engaging web platform highlighting a summer camp event's activities and registration details.",
+    image: "https://i.pinimg.com/564x/2c/e2/a7/2ce2a7edea0aabbf0569b26ca2aa96ba.jpg",
+    technologies: ["React", "Django"],
+  },
+  {
+    title: "Nottion",
+    description: "A versatile and collaborative note-taking app offering organization and seamless team communication.",
+    image: "https://i.pinimg.com/564x/92/ff/50/92ff500c05312ce2ba4f6f7317b9213b.jpg",
+    technologies: ["React", "Django"],
+  },
+  {
+    title: "3D Data Analysis",
+    description: "A data analysis tool using 3D visualizations to offer insights and better understanding of complex data sets.",
+    image: "https://i.pinimg.com/564x/34/2d/6d/342d6d33e53c5eb29a02bf011ce0cdfd.jpg",
+    technologies: ["React", "Python"],
+  },
+];
+
+export default projectsData;
