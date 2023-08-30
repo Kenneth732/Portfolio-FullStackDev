1510516fe0bb85d154850287ee9bcb007b79846a
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-30 13:24:05 +0300

    media query navbar links routers style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="copyright">
+    <p>Copyright @ Kenneth. Made by <i className='fas fa-heart'></i> CodeWise </p>
+</div>
+  )
+}
+
+export default Footer
